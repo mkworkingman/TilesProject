eb35151d50acc4f5826effeb4634a1d6801605d9
mkworkingman <mkworkingman@gmail.com>
2026-06-18 23:13:55 +0200

    Return useState for provider

--- src/store/StoreProvider.tsx
+++ src/store/StoreProvider.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { Provider } from 'react-redux'
 import { makeStore } from './index'
+import { useState } from 'react'
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
-    const store = makeStore()
+    const [store] = useState(makeStore)
+
     return <Provider store={store}>{children}</Provider>
 }
